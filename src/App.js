362171534ade52a081b2a8c7b79f0fe7e2341ae2
PDOUU <58252790+pdouu@users.noreply.github.com>
2020-01-13 13:52:19 +0300

    adding files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,94 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+//Quotes json
+const quoteURL =
+  "https://raw.githubusercontent.com/pdouu/random-quote-machine/master/quotes.json";
+
+//Background color gradient
+const backgroundList = [
+  "peach-gradient",
+  "aqua-gradient",
+  "warm-flame-gradient",
+  "young-passion-gradient",
+  "rainy-ashville-gradient",
+  "sunny-morning-gradient",
+  "blue-gradient"
+];
 
 function App() {
+  //States for background quote and quote author
+  const [appBG, setAppBG] = useState("blue-gradient");
+  const [quote, setQuote] = useState("");
+  const [author, setAuthor] = useState("");
+
+  //Getting random quotes on page reload or first load
+  useEffect(() => {
+    getRandomQuoteData();
+  }, []);
+
+  //Getting random quotes and random background
+  //On reload or on click
+  const getRandomQuoteData = () => {
+    //Setting two numbers for getting random background and quotes
+    let randomQuoteIndex = Math.floor(Math.random() * 102);
+    let randomBackgroundIndex = Math.floor(Math.random() * 6);
+    console.log(randomBackgroundIndex);
+
+    //Getting quotes from given json.
+    //Selecting random quote and its author by random number.
+    fetch(quoteURL)
+      .then(quoteResponse => {
+        return quoteResponse.json();
+      })
+      .then(quoteData => {
+        setQuote(quoteData[randomQuoteIndex].quote);
+        setAuthor(quoteData[randomQuoteIndex].author);
+        setAppBG(backgroundList[randomBackgroundIndex]);
+      })
+      .catch(quoteError => {
+        console.log(quoteError);
+      });
+  };
+  //Render
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="dis-container" className={appBG + " container-fluid vh-100"}>
+      <div id="quote-container" className="row justify-content-center h-100">
+        <div id="quote-box" className="col-4 text-center my-auto h-25">
+          <div id="text" className="row text-dark h2 justify-content-center">
+            <p>{quote}</p>
+          </div>
+          <div id="author" className="row text-dark h4 justify-content-end">
+            <p>{"- " + author}</p>
+          </div>
+          <div
+            id="buttons-container"
+            className="row align-items-end justify-content-between mt-5"
+          >
+            <a
+              id="tweet-quote"
+              href={
+                "https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=" +
+                quote +
+                "-" +
+                author
+              }
+              target="_blank"
+              rel="noopener noreferrer"
+              className="btn btn-lg peach-gradient "
+            >
+              Tweet
+            </a>
+            <button
+              id="new-quote"
+              className="btn btn-lg purple-gradient"
+              onClick={getRandomQuoteData}
+            >
+              New Quote
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
